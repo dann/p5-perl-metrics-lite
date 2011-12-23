18de9a53c9040cee5ba2c43f4458449ec04a32e3
Dann <techmemo@gmail.com>
2011-12-23 23:30:05 +0900

    changed default value

--- lib/Perl/Metrics/Lite.pm
+++ lib/Perl/Metrics/Lite.pm
@@ -7,7 +7,7 @@ use Perl::Metrics::Lite::Report::Text;
 use Perl::Metrics::Lite::Analysis;
 use Perl::Metrics::Lite::Analysis::File;
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 sub new {
     my ( $class, %args ) = @_;
