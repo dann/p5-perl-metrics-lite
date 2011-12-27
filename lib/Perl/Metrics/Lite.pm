e98812aee87c77493ca27cb2dcf472323a404a1d
Dann <techmemo@gmail.com>
2011-12-27 20:31:58 +0900

    0.06

--- lib/Perl/Metrics/Lite.pm
+++ lib/Perl/Metrics/Lite.pm
@@ -7,7 +7,7 @@ use Perl::Metrics::Lite::Report::Text;
 use Perl::Metrics::Lite::Analysis;
 use Perl::Metrics::Lite::Analysis::File;
 
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 
 sub new {
     my ( $class, %args ) = @_;
